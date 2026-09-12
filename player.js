9e168f8c74a80b7106f438d5b8f46f0544be3007
Raghu <singhraghuraj072@gmail.com>
2026-09-12 11:56:01 +0530

    feat: add animation to walk of players

--- player.js
+++ player.js
@@ -13,28 +13,35 @@ class Player {
         this.isGrounded = false;
         this.controls = controls;
         
-        
         this.sprites = {
             idle: new Image(),
-            walk: new Image(),
             jump: new Image(),
             crawl: new Image(),
             punch: new Image(),
             kick: new Image()
         };
-
         
         this.sprites.idle.src = `${spriteFolder}/idle.png`;
-        this.sprites.walk.src = `${spriteFolder}/idle.png`;
         this.sprites.jump.src = `${spriteFolder}/jump.png`;
         this.sprites.crawl.src = `${spriteFolder}/crawl.png`;
         this.sprites.punch.src = `${spriteFolder}/punch.png`;
         this.sprites.kick.src = `${spriteFolder}/kick.png`;
 
+        this.walkFrames = [];
+        for (let i = 1; i <= 10; i++) {
+            let img = new Image();
+            let paddedNum = String(i).padStart(4, '0');
+            img.src = `${spriteFolder}/walk/frame_${paddedNum}.png`;
+            this.walkFrames.push(img);
+        }
+        this.walkFrameIndex = 0;
+        this.walkTimer = 0;
+        this.walkSpeed = 6;
+
         this.currentImg = this.sprites.idle;
         this.actionTimer = 0; 
+        this.facing = 'right'; 
     }
-    
 
     handleInput(keys) {
         if (this.actionTimer > 0) {
@@ -46,7 +53,7 @@ class Player {
         this.height = 400;
         this.vx = 0;
         this.currentImg = this.sprites.idle; 
-        let oldHeight = this.height;
+        let isWalking = false;
 
         if (keys[this.controls.punch]) {
             this.currentImg = this.sprites.punch;
@@ -62,23 +69,26 @@ class Player {
             return;
         }
 
-        
-        // if (keys[this.controls.crawl] && this.isGrounded) {
-        //     this.currentImg = this.sprites.crawl;
-        //     this.height = 200;  
-        //     this.y += 500;            
-        //     return;
-        // } else {
-        //     this.height = 400; 
-        // }
-
         if (keys[this.controls.left]) {
             this.vx = -this.speed;
-            this.currentImg = this.sprites.walk;
+            this.facing = 'left';
+            isWalking = true;
         }
         if (keys[this.controls.right]) {
             this.vx = this.speed;
-            this.currentImg = this.sprites.walk;
+            this.facing = 'right';
+            isWalking = true;
+        }
+
+        if (isWalking && this.isGrounded) {
+            this.walkTimer++;
+            if (this.walkTimer >= this.walkSpeed) {
+                this.walkTimer = 0;
+                this.walkFrameIndex = (this.walkFrameIndex + 1) % this.walkFrames.length;
+            }
+        } else {
+            this.walkFrameIndex = 0;
+            this.walkTimer = 0;
         }
 
         if (keys[this.controls.jump] && this.isGrounded) {
@@ -88,7 +98,6 @@ class Player {
 
         if (!this.isGrounded) {
             this.currentImg = this.sprites.jump;
-            
         }
     }
 
@@ -111,10 +120,30 @@ class Player {
         ctx.ellipse(this.x + this.width / 2, this.y + this.height, 35, 8, 0, 0, Math.PI * 2);
         ctx.fill();
 
-        ctx.drawImage(this.currentImg, this.x, this.y, this.width, this.height);
+        ctx.save();
+
+        let drawingImg = this.currentImg;
+        let isWalkingNow = (this.vx !== 0 && this.isGrounded && this.currentImg === this.sprites.idle);
+
+        if (isWalkingNow && this.walkFrames.length > 0) {
+            drawingImg = this.walkFrames[this.walkFrameIndex] || this.sprites.idle;
+        }
+
+        if (drawingImg && drawingImg.complete && drawingImg.naturalWidth > 0) {
+            if (this.facing === 'left') {
+                ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+                ctx.scale(-1, 1);
+                ctx.drawImage(drawingImg, -this.width / 2, -this.height / 2, this.width, this.height);
+            } else {
+                ctx.drawImage(drawingImg, this.x, this.y, this.width, this.height);
+            }
+        }
+
+        ctx.restore();
+
         ctx.textAlign = "center";
         ctx.fillStyle = "#ff0000";
         ctx.font = "bold 14px Arial";
-        ctx.fillText(this.name, this.x, this.y - 10);
+        ctx.fillText(this.name, this.x + this.width / 2, this.y - 10);
     }
 }
